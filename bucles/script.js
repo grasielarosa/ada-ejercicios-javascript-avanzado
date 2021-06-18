63dd24f3020ce85ddbfe80657d55a7ad4d1ae64c
grasielarosa <grasielarosaa@gmail.com>
2021-06-18 18:07:23 +0200

    bucles - ejercicio 1

---
+++ bucles/script.js
@@ -0,0 +1,6 @@
+// ---------- ejercicio 1 - Obtener Indice
+// const obtenerIndice = (valor, array) => {
+//     return array.indexOf(valor); // --- el index of devuelve por defecto el -1 cuando no encuentra el valor y la posicion si lo encuentra 
+//   }
+// console.log(obtenerIndice(5, [5, 7, 12, 34, 54, 2, 12]));
+// console.log(obtenerIndice(83, [5, 7, 12, 34, 54, 2, 12]));
