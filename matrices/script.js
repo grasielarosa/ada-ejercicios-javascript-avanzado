bb0e61ef610c33b547b4d1d3f73ca707731ba5e9
grasielarosa <grasielarosaa@gmail.com>
2021-06-23 13:09:11 +0200

    ejercicios 1, 2 y 3

---
+++ matrices/script.js
@@ -0,0 +1,42 @@
+// --------------- Ejercicio 1: Obtener número mayor
+// const obtenerNumeroMayor = (matriz) => {
+//     let mayor = 0;
+//     for (let i=0; i<matriz.length; i++){
+//         for (let j=0; j<matriz[i].length; j++){
+//             if (matriz[i][j]>mayor){
+//                 mayor = matriz[i][j];
+//             }
+//         }
+//     }
+//     return mayor;
+// }
+
+//  console.log(obtenerNumeroMayor([[2, 7, 12, 1],[8, 23], [9, 45, 7],[22, 3, 24, 4],]));
+
+// --------------- Ejercicio 2: Sumar
+
+// const sumar = (matriz) => {
+//     let suma = 0;
+//     for (let i=0; i<matriz.length; i++){
+//         for (let j=0; j<matriz[i].length; j++){
+//             suma = suma + matriz[i][j];
+//         }
+//     }
+//     return suma;
+// }
+
+//  console.log(sumar([[4, 5],[2, 7, 1],[8, 10],]));
+
+// --------------- Ejercicio 3: Es matriz cuadrada
+
+// const esMatrizCuadrada = (matriz) => {
+//     for (let arrays of matriz){
+//       for (let i=0; i<arrays.length; i++){
+//         return arrays.length === matriz.length;
+//       }
+//     }
+//   }
+
+//  console.log(esMatrizCuadrada([[4, 5],[2, 7, 1],[8, 10],]) );
+//  console.log(esMatrizCuadrada([[4, 5, 9],[2, 7, 1],[8, 10, 5],]));
+ 
