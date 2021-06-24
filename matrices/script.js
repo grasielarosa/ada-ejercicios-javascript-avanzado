ae80aedfba87cc46dcd99960a51c5a1062fb484f
grasielarosa <grasielarosaa@gmail.com>
2021-06-24 10:45:37 +0200

    matrizes - ejercicio 5

--- matrices/script.js
+++ matrices/script.js
@@ -43,7 +43,7 @@
 
 // --------------- Ejercicio 4: Generar grilla
     // --- creamos los 3 parámetros. Luego declaramos un array vacío (grilla) que va recibir la cantidad de filas que el primer for encuentre. Para cada fila, el segundo for creará índices (columna), y en el array columna vacío vá poniendo los ítems de manera aleatória. En el array grilla, subimos lo que se encuentre en columna. Más graficamente: llego a la linea 1, entro en la columna uno, hago el push de los items aleatórios, salgo de la columna uno, hago el push de esa columna para la grilla. Voy para la línea dos y así por delante. 
-    
+
 
 // const generarGrilla = (filas, columnas, items) => {
 //     let grilla = [];
@@ -59,3 +59,21 @@
 
 // console.log(generarGrilla(2, 3, [1, 2]));
 // console.log(generarGrilla(3, 3, ['a', 'b', 'c']));
+
+
+// --------------- Ejercicio 5:  Generar matriz escalonada
+    // --- el array de matriz está vacío para al final recibir las filas. El for de indice j entra en cada indice i, filas no necesita length porque ya es numero entero. Así que en el indice [0] de filas, el j hace push de 0 + 1, entonces la primera fila tiene apenas un elemento. En la segunda volta es el i=[1], entonces 1+1. Suben dos zeros para la fila. Y asi por delante. Al final, hacemos el push de todas las filas para la matriz.
+
+// const generarMatrizEscalonada = (filas) => {
+//     let matriz = [];
+//     for (let i=0; i<filas; i++){
+//         let fila = [];
+//         for (let j=0; j<i+1; j++){
+//             fila.push(0);
+//         }
+//         matriz.push(fila);
+//     }
+//     return matriz;
+// }
+//  console.log(generarMatrizEscalonada(3));
+//  console.log(generarMatrizEscalonada(25));
